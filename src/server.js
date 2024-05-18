e2f64c211371b0e3b1669aa02c937019d9e5fe3b
ThiagoThalisson <atomic-feast@proton.me>
2024-05-18 09:17:57 -0300

    Can now save and list users in our /users route using JSON

--- src/server.js
+++ src/server.js
@@ -1,14 +1,24 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
   console.log(method, url);
   if (method === "GET" && "/users") {
-    return res.end("Listagem de usuários");
+    return res
+      .setHeader("Content-type", "application/json")
+      .end(JSON.stringify(users));
   }
 
   if (method === "POST" && "/users") {
+    users.push({
+      id: "1",
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
     return res.end("Criação de usuário");
   }
 
