ecc310c031b836c2908c78b09d0826bb5ab72e91
ThiagoThalisson <atomic-feast@proton.me>
2024-05-18 08:36:11 -0300

    Adds distinction between listing and creating users with methods

--- src/server.js
+++ src/server.js
@@ -1,6 +1,17 @@
 import http from "node:http";
 
 const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  console.log(method, url);
+  if (method === "GET" && "/users") {
+    return res.end("Listagem de usuários");
+  }
+
+  if (method === "POST" && "/users") {
+    return res.end("Criação de usuário");
+  }
+
   return res.end("Hello World");
 });
 
